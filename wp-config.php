7d39fb490f742d2f4d9cd9507e65a5459ead3203
Pau Iranzo <paugnu@gmail.com>
2015-11-08 21:06:39 +0100

    #45 update composer.json to call wp-cli + update the wp-config.php file to be compatible with wp-cli

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,11 @@
 <?php
 
+require_once(ABSPATH . '../../conf/wordpress/wp-config.php');
+
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Location of your WordPress configuration. */
-require_once(ABSPATH . '../../conf/wordpress/wp-config.php');
+
+require_once(ABSPATH . 'wp-settings.php');
