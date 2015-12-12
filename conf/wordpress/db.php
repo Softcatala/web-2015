874ab83018cfc74cb85502b3717c9048ffee6900
Pau Iranzo <paugnu@gmail.com>
2015-12-12 23:42:40 +0100

    Moved base wordpress conf files to specific directory

---
+++ conf/wordpress/db.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'db_name');
+
+/** MySQL database username */
+define('DB_USER', 'db_user');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'db_pass');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         '&Rl)XdT-+Viw>g=9FQL.5b11O|I!e9y;25MOhJPCNVzZa,.8oE,n!6bt]zV JSG*');
+define('SECURE_AUTH_KEY',  'zNGUN=0>qpw55{UWZruSS*gp:bfyCY3# *u)6:NSAAc.Kt&:kUxg63!|F7$WSHhX');
+define('LOGGED_IN_KEY',    '|$S]I2q,E4:aBgWY]y3c&Vf`vSZK|+U?s,_}LZ&ZfrXby!~?{5f!+CJvV+XGV 0B');
+define('NONCE_KEY',        'G<`PqHmQJR#AZh+BYA:-t2/n5]H-@T>tNpkr$jq]]M2Ce.kptr2Sy2^|@;[bO^&Z');
+define('AUTH_SALT',        ' IMJ:*^[CgXDo(R{ev=%}PPCZKUXXUsnI*Fd[aII|7eQ}&70U3Q*[8zCVf{]=O[]');
+define('SECURE_AUTH_SALT', 'ZbUq1nSW8o%3y`V_k^h1[lsAX]L|ve*iz3vs-ClwzZ%z.{/dxy52~XKkC@N&)Xk;');
+define('LOGGED_IN_SALT',   'A%bc$3J|=SLMuql>#(z%O+;GS><?-Z6OJtD<C#ieKMw3Z|;ru7-ic7^[V=Ud1R7C');
+define('NONCE_SALT',       '>&xF}J2AD{*bvdA#mT0bJ=LBLiQWn<N?r*FCDubYfU41k06Eq,Ew{0|yAQ$JVK%y');
