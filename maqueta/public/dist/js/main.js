a9e6b38468fd46ab5e565534a8dcbfb14634f27d
Pau Iranzo <paugnu@gmail.com>
2016-04-18 18:44:06 +0200

    #135 automatically switch to most downloads users so tab

--- maqueta/public/dist/js/main.js
+++ maqueta/public/dist/js/main.js
@@ -4543,8 +4543,19 @@ if ( typeof define === 'function' && define.amd ) {
                 jQuery(element).addClass('active');
             }
             jQuery("#navbar-usuari-mobile").removeClass('active');
+        } else {
+            var OSName="Unknown OS";
+            if (navigator.userAgent.indexOf("Win") != -1) OSName="windows";
+            else if (navigator.userAgent.indexOf("Mac") != -1) OSName="osx";
+            else if (navigator.userAgent.indexOf("Android") != -1) OSName="android";
+            else if (navigator.userAgent.indexOf("Linux") != -1) OSName="linux";
+            else if (navigator.userAgent.indexOf("iPad") != -1 || navigator.userAgent.indexOf("iPhone") != -1 || navigator.userAgent.indexOf("iPod") != -1) OSName="ios";
+
+            jQuery(".tab-"+OSName+" > a").trigger('click');
         }
     });
+
+
 
 })(jQuery, document, window, ResponsiveBootstrapToolkit);
 
